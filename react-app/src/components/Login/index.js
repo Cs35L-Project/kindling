415e679a192852b62836f8ff1e9aa0673ebdb752
jennkim0409 <93408606+jennkim0409@users.noreply.github.com>
2021-11-18 04:24:27 -0800

    Add files via upload
    Add the Login (and Signup) component

---
+++ react-app/src/components/Login/index.js
@@ -0,0 +1,184 @@
+import React from 'react';
+import Select from 'react-select';
+import './index.css';
+import { BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import Creatable from 'react-select/creatable';
+
+export default function Login() {
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<LoginPage/>} />
+                <Route path="/Signup" element={<Signup/>} />
+            </Routes>
+        </BrowserRouter> 
+    );
+}
+
+const LoginPage = props => {
+    return (
+        <div className="login-wrapper">
+            <h1>Welcome Back! Log In</h1>
+            <form>
+                <label>
+                    <p>Username</p>
+                    <input type="text" className="form-control" placeholder="Enter username"/>
+                </label>
+                <label>
+                    <p>Password</p>
+                    <input type="password" className="form-control" placeholder="Enter password"/>
+                </label>
+                <div className="custom-control custom-checkbox">
+                    <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                    <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                </div>
+                <div>
+                    <button type="submit" className="btn btn-primary btn-block">Submit</button>
+                    <p className="forgot-password text-right">
+                        Forgot <a href="#">password?</a>
+                    </p>
+                </div>
+                <div>
+                    <Link to="/Signup" className="btn btn-primary">Signup</Link>
+                </div>
+            </form>
+        </div>
+    );
+}
+
+const sports = [
+    { label: 'Basketball', value: 'Basketball'},
+    { label: 'Soccer', value: 'Soccer'},
+    { label: 'Baseball', value: 'Baseball'},
+    { label: 'Tennis', value: 'Tennis'},
+    { label: 'Volleyball', value: 'Volleyball'},
+    { label: 'Dance', value: 'Dance'},
+    { label: 'Gymnastics', value: 'Gymnastics'},
+    { label: 'Football', value: 'Football'},
+    { label: 'Cross Country', value: 'Cross Country'},
+    { label: 'Hockey', value: 'Hockey'},
+    { label: 'Golf', value: 'Golf'},
+    { label: 'Swimming', value: 'Swimming'},
+];
+const hobbies = [
+    { label: 'Cooking', value: 'Cooking'},
+    { label: 'Traveling', value: 'Traveling'},
+    { label: 'Sewing', value: 'Sewing'},
+    { label: 'Video Games', value: 'Video Games'},
+    { label: 'Eating', value: 'Eating'},
+    { label: 'Pets', value: 'Pets'},
+    { label: 'Reading/Writing', value: 'Reading/Writing'},
+    { label: 'Arts and Crafts', value: 'Arts and Crafts'},
+    { label: 'Technology', value: 'Technology'},
+    { label: 'Gardening', value: 'Gardening'},
+    { label: 'Socializing', value: 'Socializing'},
+    { label: 'Photography', value: 'Photography'},
+    { label: 'Health and Fitness', value: 'Health and Fitness'},
+    { label: 'Anime and Manga', value: 'Anime and Manga'}
+];
+const personality = [
+    { label: 'INTJ', value: 'INTJ'},
+    { label: 'INTP', value: 'INTP'},
+    { label: 'ENTJ', value: 'ENTJ'},
+    { label: 'ENTP', value: 'ENTP'},
+    { label: 'INFJ', value: 'INFJ'},
+    { label: 'INFP', value: 'INFP'},
+    { label: 'ENFJ', value: 'ENFJ'},
+    { label: 'ENFP', value: 'ENFP'},
+    { label: 'ISTJ', value: 'ISTJ'},
+    { label: 'ISFJ', value: 'ISFJ'},
+    { label: 'ESTJ', value: 'ESTJ'},
+    { label: 'ESFJ', value: 'ESFJ'},
+    { label: 'ISTP', value: 'ISTP'},
+    { label: 'ISFP', value: 'ISFP'},
+    { label: 'ESTP', value: 'ESTP'},
+    { label: 'ESFP', value: 'ESFP'},
+    { label: 'Not sure', value: 'Not sure'}
+];
+const music = [
+    { label: 'Alternative', value: 'Alternative'},
+    { label: 'Anime', value: 'Anime'},
+    { label: 'Blues', value: 'Blues'},
+    { label: 'Classical', value: 'Classical'},
+    { label: 'Country', value: 'Country'},
+    { label: 'Disney', value: 'Disney'},
+    { label: 'Hip-Hop/Rap', value: 'Hip-Hop/Rap'},
+    { label: 'Indie', value: 'Indie'},
+    { label: 'J-Pop', value: 'J-Pop'},
+    { label: 'Jazz', value: 'Jazz'},
+    { label: 'K-Pop', value: 'K-Pop'},
+    { label: 'Latin', value: 'Latin'},
+    { label: 'Metal', value: 'Metal'},
+    { label: 'Pop', value: 'Pop'},
+    { label: 'R&B/Soul', value: 'R&B/Soul'},
+    { label: 'Rock', value: 'Rock'},
+]
+
+const Signup = props => {
+    return (
+        <form>
+                <h3>Don't Have an Account?</h3>
+
+                <div className="form-group">
+                    <label>First Name</label>
+                    <input type="text" className="form-control" placeholder="Enter first name" />
+                </div>
+
+                <div className="form-group">
+                    <label>Last Name</label>
+                    <input type="text" className="form-control" placeholder="Enter last name" />
+                </div>
+
+                <div className="form-group">
+                    <label>Gender</label>
+                    <button type="radio" value="Male" name="gender"/>Male
+                    <button type="radio" value="Female" name="gender"/>Female
+                    <button type="radio" value="Nonbinary" name="gender"/>Nonbinary
+                    <button type="radio" value="Prefer Not to Say" name="gender"/>Prefer Not to Say
+                </div>
+
+                
+                <div className="form-group">
+                    <label>Email Address</label>
+                    <input type="email" className="form-control" placeholder="Enter email" />
+                </div>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password" />
+                </div>
+
+                <div className="form-group">
+                    <label>What are your hobbies?</label>
+                        <Creatable options={hobbies}
+                            isMulti
+                            onChange={(opt, meta) => console.log(opt, meta)}
+                        />
+                    <label>What sports do you like?</label>
+                        <Creatable options={sports}
+                            isMulti
+                            onChange={(opt, meta) => console.log(opt, meta)}
+                        />
+                    <label>What music do you like?</label>
+                        <Creatable options={music}
+                            isMulti
+                            onChange={(opt, meta) => console.log(opt, meta)}
+                        />
+                    <label>What is your personality type?</label>
+                        <Creatable options={personality}
+                            isMulti
+                            onChange={(opt, meta) => console.log(opt, meta)}
+                        />
+                </div>
+
+                <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
+                <p className="forgot-password text-right">
+                    Already registered <a href="/">Sign in?</a>
+                </p>
+            </form>
+    );
+}
+
+
+
+
