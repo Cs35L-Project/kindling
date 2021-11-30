cb3685b6f1ac6920c86f1ee981f4901299b44960
Darren Huang <huangdarren1106@gmail.com>
2021-11-30 11:59:16 -0800

    split matching into 3 loops

--- react-app/src/components/profile/helper.js
+++ react-app/src/components/profile/helper.js
@@ -1,41 +1,86 @@
 import axios from "axios";
 
-generateFeed(userID){
-    var feed = [] 
-    const availUsers = getUsers(); //contains user ID's 
-    const likedUser = [];
-    const userInterests = //get userID's interests
-    //get a bunch of users
+export function generateFeed(userID){
+    var feed = []; 
+    var matchingUser = fetch("http://localhost:4000/api/users/:" + userID) //get user object using userID
+    .then(response => response.json())
+    .then(function(data)
+    {
+        return data;
+    })
+    .catch(function(error)
+    {
 
-        //check if usersID[], add userIDs that liked user to likedUser list
-        var i = 0;
-        while(feed.length < 10 && i < availUsers.length)
+    })
+
+    //contains list of all users 
+    const availUsers = fetch("http://localhost:4000/api/users/")
+    .then(response => response.json())
+    .then(function(data)
+    {
+        return data.slice();
+    })
+    .catch(function(error)
+    {
+
+    })
+
+    //add mutually liked users
+    var a = 0;
+    while(feed.length < 10 && a < matchingUser.likedList.length)
+    {
+        var currUser = fetch("http://localhost:4000/api/users/:" + matchingUser.likedList[a]) //get currUser based on userID of likedList[i]
+        .then(response => response.json())
+        .then(function(data)
         {
-            const currUserID = availUsers[i].id; //get userID
-            var likedUser = false; //bool that checks if currUser liked user
-            var j;
-            for(j = 0; j<availUsers[i].likedList.length; j++){
-                if(availUsers[i].likedList[j] == userID)
-                {
-                    likedUser = true;
-                }
-            }
-            if(likedUser)
-            {
-                feed.unshift(currUserID);
-            }
-            else //look at interests
-            {
-                const currUserInterests = availUsers[i].interests //list of user interests
-                //make array of matching interests
-                const filteredStrArray = userInterests.filter(value => currUserInterests.includes(value)).filter((value, index, self) => self.indexOf(value) === index);
-                if(filteredStrArray.length > 2)
-                {
-                    feed.push(currUserID);
-                }
-            }
-            i++;
+            return data;
+        })
+        .catch(function(error)
+        {
+    
+        })
+        if(currUser.likedList.includes(userID))
+        {            
+            //add to feed
+            feed.unshift(currUserID);
         }
+        a++;
+    }
 
+    //add by interests
+    const userInterests = matchingUser.interests//get userID's interests        
+    //add users with common interests
+    var i = 0;
+    while(feed.length < 10 && i < availUsers.length)
+    {   
+        const currUserID = availUsers[i].id; //get userID of current user
+        if(userID == currUserID || feed.includes(currUserID)) //skip if user matched with itself or currUser is already in list
+        {
+            i++;
+            continue;
+        }
+        const currUserInterests = availUsers[i].interests //list of user interests
+                //make array of matching interests
+        const filteredStrArray = userInterests.filter(value => currUserInterests.includes(value)).filter((value, index, self) => self.indexOf(value) === index);
+        if(filteredStrArray.length > 2)
+        {
+            feed.push(currUserID);
+        }
+        i++;
+    }
 
-};
+    //fill up rest of feed
+    var c = 0;
+    while(feed.length < 10 && c < availUsers.length)
+    {
+        const currUserID = availUsers[c].id;
+        if(userID == currUserID || feed.includes(currUserID)) //skip if user matched with itself or currUser is already in list
+        {
+            i++;
+            continue;
+        }
+        feed.push(currUserID)
+    }
+
+    return feed;
+}
