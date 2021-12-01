a1716669991fdd718a0704a8fd0aa23c3fe559f8
elizsetian <93408601+elizsetian@users.noreply.github.com>
2021-11-30 20:35:05 -0800

    Add files via upload

---
+++ server/app/routes/auth.routes.js
@@ -0,0 +1,26 @@
+const { verifySignUp } = require("../middleware");
+const controller = require("../controllers/auth.controller");
+
+module.exports = function (app) {
+    app.use(function (req, res, next) {
+        res.header(
+            "Access-Control-Allow-Headers",
+            "x-access-token, Origin, Content-Type, Accept"
+        );
+        next();
+    });
+
+    var router = require("express").Router();
+
+    // Signup the User
+    router.post(
+        "/signup",
+        verifySignUp.checkDuplicateUsernameOrEmail,
+        controller.signup
+    );
+
+    // Signin the User
+    router.post("/signin", controller.signin);
+
+    app.use("/api/auth", router);
+};
