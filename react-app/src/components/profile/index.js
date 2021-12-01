7a07111d5ebf75df75e1b5d7d3ff50278c5fba76
jennkim0409 <93408606+jennkim0409@users.noreply.github.com>
2021-12-01 04:47:04 -0800

    Update index.js

--- react-app/src/components/profile/index.js
+++ react-app/src/components/profile/index.js
@@ -4,40 +4,49 @@ import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 
 const Profile = props => {      //props should be a unique id corresponding to the user
-    const [profile, setProfile] = useState([{
-        Name: "null", 
-        Description: "null", 
-        UID: props.userID, 
-        User: props.root,
-        Matches: [], 
-        Potentials: []}]);
+    //const [profile, setProfile] = useState();
+    
+    const [profile, setProfile] = useState({
+        Name: null, 
+        Description: null, 
+        Interests: null,
+        UID: props.userID,
+        init:true});
+    
     // Below promise will only be functional once backend is serving data
-    /*
-    axios.get(`http://localhost:4000/api/?profile=${props.userID}`)
-    .then((res) => {
-        var response = res.data;
-        let data = [];
-        setProfile(data);
-    })
-    .catch((e) => {
-        console.log(e);
-        console.log("cannot connect to server");
-    })
-    */
+    if(profile.init){
+        axios.get(`http://localhost:4000/api/users/${props.userID}`)
+        .then((res) => {
+            var response = res.data;
+            let data = {
+                Name: response.fullName, 
+                Description: response.bio, 
+                Interests: response.interests,
+                UID: props.userID, 
+            };
+            setProfile(data);
+        })
+        .catch((e) => {
+            console.log(e);
+            console.log("cannot connect to server");
+        })
+    }
+
     if(props.size=="full"){
         return (
-                <div className="rectangle">
-                    <h1>NAME</h1>
-                    <h2>PICTURE</h2>
-                    <h3>DESCRIPTION</h3>
-                    <h4>USER ID: {props.userID}</h4>
-                </div>
-        );
+            <div className="rectangle">
+                <h1>{profile.Name}</h1>
+                <h2>PICTURE</h2>
+                <h3>{profile.Description}</h3>
+                <h4>{profile.Interests}</h4>
+                <h5>USER ID: {props.userID}</h5>
+            </div>
+            );
     }
     else{
         return (
             <div className="square">
-                <h1>NAME</h1>
+                <h1>{profile.Name}</h1>
                 <h2>PICTURE</h2>
                 <h3>USER ID: {props.userID}</h3>
             </div>
