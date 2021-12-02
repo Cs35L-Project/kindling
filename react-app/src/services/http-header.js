33e55a5f745f10ff3b9bb763f204f0675755e48d
Clement Nguyen <nguyenclementp@gmail.com>
2021-12-01 20:15:35 -0800

    Added missing http-header
    Added missing http-header

---
+++ react-app/src/services/http-header.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+import AuthService from "auth.service.js";
+
+export default axios.create({
+  baseURL: "http://localhost:4000/api",
+  headers: {
+    "Content-type": "application/json"
+  }
+});
