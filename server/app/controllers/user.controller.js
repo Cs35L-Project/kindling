05bfc76119805b48a7122ae4d63da8be5ced2026
aditsharma-proejcts <ben10raceagainsttime@gmail.com>
2021-12-01 21:17:57 -0800

    Small change to make functional

--- server/app/controllers/user.controller.js
+++ server/app/controllers/user.controller.js
@@ -92,7 +92,7 @@ exports.upload = (req, res) => {
         }
 
         User.update({ avatar: req.file.filename }, {
-            where: { id: req.body.id }
+            where: { id: req.body.body }
         })
             .then(num => {
                 if (num == 1) {
@@ -112,7 +112,6 @@ exports.upload = (req, res) => {
             });
 
     } catch (error) {
-        console.log(error);
         return res.send({ message: `Error uploading image: ${error}` });
     }
 }
